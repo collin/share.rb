e1ab1e911b521eeefbe643c397e05ce46069351b
Collin Miller <collintmiller@gmail.com>
2012-08-15 16:26:40 -0500

    added first spec for Action class

--- lib/share.rb
+++ lib/share.rb
@@ -1,2 +1,3 @@
 module Share
+  require "share/action"
 end
--- lib/share/action.rb
+++ lib/share/action.rb
@@ -1,14 +1,16 @@
 module Share
   class Action
-    CONNECT = ['connect'].freeze!
-    CREATE = ['create'].freeze!
-    READ = ['get snapshot', 'get ops', 'open'].freeze!
-    UPDATE = ['submit op', 'submit meta'].freeze!
-    DELETE = ['delete'].freeze!
+    CONNECT = ['connect'].freeze
+    CREATE = ['create'].freeze
+    READ = ['get snapshot', 'get ops', 'open'].freeze
+    UPDATE = ['submit op', 'submit meta'].freeze
+    DELETE = ['delete'].freeze
 
     ACTIONS = [
       CONNECT, CREATE, READ, UPDATE, DELETE
-    ].freeze!
+    ].freeze
+
+    attr_reader :type
 
     def initialize(data, name)
       @data = data
